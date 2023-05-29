a24310a8e0b79a6e651486e759c419f7640d599b
doganeraylin <doganeraylinn@gmail.com>
2023-05-29 11:55:22 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;0,900;1,300&display=swap');
+@import "@/styles/variables.scss";
+@import "@/styles/globals.scss";
+</style>
