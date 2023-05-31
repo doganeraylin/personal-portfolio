da5d68d05220dcf0ac340c671cf713a5a5de2cc1
doganeraylin <doganeraylinn@gmail.com>
2023-05-31 09:05:55 +0200

    created tech stack items layout

---
+++ components/About/TechStack.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="tech_stack">
+            <img src="/react.png">
+            <p>Next.js</p>
+        </div>
+        <div class="tech_stack">
+            <img src="/react.png">
+            <p>Next.js</p>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@import "@/styles/variables.scss";
+
+.container {
+    display: flex;
+    justify-content: space-around;
+    gap: 1rem;
+}
+
+.tech_stack {
+    border: 1px solid $black;
+    text-align: center;
+    padding: 1rem;
+    img {
+        max-width: 150px;
+    }
+    p {
+        font-size: 1.5rem;
+        opacity: 0.5;
+        margin: 0.5rem;
+    }
+}
+</style>
