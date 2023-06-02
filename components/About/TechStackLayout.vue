9074808202468a3a95a4591915e8af6332b58eab
doganeraylin <doganeraylinn@gmail.com>
2023-06-02 09:24:16 +0200

    added media queries

--- components/About/TechStackLayout.vue
+++ components/About/TechStackLayout.vue
@@ -19,24 +19,26 @@
 
 .container {
     display: grid;
-    grid-template-columns: repeat(8, 1fr);
+    grid-template-columns: repeat(7, 1fr);
     > * {
         border: 2px solid $black;
     }
     &__blue {
         grid-column: 1 / 2;
         background-color: $primary-blue;
+
     }
     &__top_transparent {
         grid-column: 2 / 8;
     }
     &__title {
-        grid-column: 2 / 8;
-        padding: 2rem 1rem;
-        font-size: 2.5rem;
-    }
-    &__right_transparent {
-        grid-column: 8 / 9;
+        grid-column: 1 / 8;
+        padding: 1rem;
+        font-size: 1.5rem;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        background-color: $primary-red;
     }
     &__left_transparent {
         grid-column: 1 / 2;
@@ -45,17 +47,71 @@
     &__description {
         grid-column: 2 / 8;
         padding: 3rem 1rem;
-        font-size: 2.5rem;
     }
     &__description p {
-        max-width: 90%;
+        max-width: 85%;
+        font-size: 2rem;
     }
+    &__blue,
+    &__top_transparent,
+    &__right_transparent,
     &__yellow {
-        grid-column: 8 / 9;
-        background-color: $primary-yellow;
+        display: none;
     }
+}
 
+@media (min-width: 768px) {
+    .container {
+        grid-template-rows: repeat(4, 0.5fr);
+        &__blue,
+        &__top_transparent,
+        &__right_transparent,
+        &__yellow {
+            display: block;
+        }
+        &__top_transparent {
+            grid-column: 2 / 8;
+        }
+        &__title {
+            grid-column: 2 / 7;
+            padding: 1rem;
+            font-size: 1.5rem;
+            text-align: center;
+            background-color: $primary-red;
+        }
+        &__right_transparent {
+            grid-column: 7 / 8;
+        }
+        &__left_transparent {
+            grid-column: 1 / 2;
+            grid-row: 3 / 5;
 
+        }
+        &__description {
+            grid-column: 2 / 7;
+            grid-row: 3 / 5;
+            padding: 3rem 1rem;
+        }
+        &__description p {
+            max-width: 80%;
+            font-size: 3rem;
+        }
+        &__yellow {
+            grid-column: 7 / 8;
+            grid-row: 3 / 5;
+            background-color: $primary-yellow;
+        }
+        &__blue {
+            grid-column: 1 / 2;
+            grid-row: 1 / 3;
+            background-color: $primary-blue;
+        }
+
+        &__title {
+            text-transform: uppercase;
+            text-align: left;
+        }
+    }   
 }
 
 
