486df07c7abcfbdcbbbbca0271fc5a8b5e6032e4
doganeraylin <doganeraylinn@gmail.com>
2023-06-02 09:28:06 +0200

    added button comp and made layout responsive

--- components/About/ProjectLayout2.vue
+++ components/About/ProjectLayout2.vue
@@ -17,7 +17,7 @@ export default {
         <div class="container__transparent_vertical"></div>
         <div class="container__colored_vertical"></div>
         <div class="container__text">
-            <h1>space tourism website</h1>
+            <h1 class="container__title">space tourism website</h1>
             <ul>
                 <li>and pushing the boundaries of frontend innovation</li>
                 <li>and pushing the boundaries of frontend innovation</li>
@@ -28,10 +28,15 @@ export default {
                 <li>and pushing the boundaries of frontend innovation</li>
                 <li>and pushing the boundaries of frontend innovation</li>
             </ul>
-            <button>see details</button>
+            <div class="container__button">
+              <Button 
+                buttonText="see details"
+                buttonClass="primary-style"
+                to=""/>
+            </div>
         </div>
-        <div class="container__screenshot">
-            <div></div> 
+        <div class="container__preview">
+            <div class="container__screenshot">img</div> 
             <div class="container__links">
                 <p><span>live link:</span> www.sthsthsth.com</p>
                 <p><span>github link:</span> www.sthsthsth-sthsth.com</p>
@@ -47,24 +52,21 @@ export default {
 
 .container {
   display: grid;
-  grid-template-columns: repeat(15, 1fr);
+  grid-template-columns: repeat(8, 1fr);
     grid-template-rows: repeat(4, 1fr);
   > * {
     border: 2px solid $black;
   }
-  &__transparent_vertical {
-    grid-column: 15 / 16;
-    grid-row: 3 / 5;
-  }
-  &__colored_vertical {
-    grid-column: 15 / 16;
-    grid-row: 1 / 3;
-    background-color: $black;
+  &__transparent_vertical,
+  &__colored_vertical,
+  &__bottom_transparent,
+  &__screenshot {
+    display: none;
   }
   &__text {
-    grid-column: 6 / 15;
+    grid-column: 1 / 9;
     grid-row: 1 / 5;
-    padding: 1rem 1.5rem;
+    padding: 1.5rem 1rem;
   }
   &__text ul {
     list-style: none;
@@ -73,21 +75,50 @@ export default {
   &__text > * {
     margin: 0.6rem;
   }
-  &__links {
-    grid-column: 1 / 6;
-  }
-  &__screenshot {
-    grid-column: 1 / 6;
-    grid-row: 1 / 4;
-  }
-  &__bottom_transparent {
-    grid-column: 1 / 6;
-  }
-  button {
-    padding: 1rem;
-    font-size: 1rem;
+  &__preview {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    grid-column: 1 / 9;
   }
 }
 
+@media (min-width: 992px) {
+.container {
+    &__transparent_vertical,
+    &__colored_vertical,
+    &__bottom_transparent,
+    &__screenshot {
+      display: block;
+    }
+    &__transparent_vertical {
+      grid-row: 1 / 3;
+    }
+    &__colored_vertical {
+      grid-row: 3 / 5;
+      background-color: $primary-red;
+    }
+    &__text {
+      grid-column: 4 / 8;
+      grid-row: 1 / 5;
+      padding: 1.5rem 1rem;
+    }
+    &__preview,
+    &__bottom_transparent {
+      grid-column: 1 / 4;
+    }
+    &__preview {
+      grid-row: 1 / 4;
+    }
+    &__bottom_transparent {
+      grid-row: 4 / 5;
+    }
+    &__title {
+      text-transform: uppercase;
+    } 
+  }
+
+}
 
 </style>
