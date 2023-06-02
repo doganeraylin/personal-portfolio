1dc5d0b80a8625af73c4a97b80f4aff7c657d12b
doganeraylin <doganeraylinn@gmail.com>
2023-06-02 09:23:35 +0200

    removed project components

--- pages/about.vue
+++ pages/about.vue
@@ -5,13 +5,6 @@
         <StatementOne/>
         <StatementTwo/>
         <StatementThree/>
-        <Journey/>
-        <TechStackLayout/>
-        <TechStack/>
-        <ProjectLayout1/>
-        <ProjectLayout2/>
-        <ProjectLayout1/>
-        <ProjectLayout2/>
         <Footer/>
     </div>
 </template>
@@ -21,24 +14,14 @@ import HelloSection from "@/components/About/HelloSection.vue"
 import StatementOne from "@/components/About/StatementOne.vue"
 import StatementTwo from "@/components/About/StatementTwo.vue"
 import StatementThree from "@/components/About/StatementThree.vue"
-import Journey from "@/components/About/Journey.vue"
-import TechStackLayout from "@/components/About/TechStackLayout.vue"
-import TechStack from "@/components/About/TechStack.vue"
-import ProjectLayout1 from "@/components/About/ProjectLayout1.vue"
-import ProjectLayout2 from "@/components/About/ProjectLayout2.vue"
+
 export default {
     components : {
         HelloSection,
         StatementOne,
         StatementTwo,
         StatementThree,
-        Journey,
-        TechStackLayout,
-        TechStack,
-        ProjectLayout1,
-        ProjectLayout2
     }
 }
-
 </script>
 
