61a96b9a35cf6352cad5ebd509d4d6f30be60b12
doganeraylin <doganeraylinn@gmail.com>
2023-06-15 10:53:26 +0200

    created dynamic routing for projects

---
+++ pages/projects/[id].vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ProjectIntro :project="project" />
+    <ProjectVideo/>
+    <ProjectOverview :project="project"/>
+    <KeyFeatures :project="project"/>
+    <FutureEnhancements :project="project"/>
+    <Screenshots :project="project"/>
+  </div>
+</template>
+
+<script setup>
+    import ProjectIntro from "@/components/ProjectDetails/ProjectIntro.vue"
+    import ProjectVideo from "@/components/ProjectDetails/ProjectVideo.vue"
+    import ProjectOverview from "@/components/ProjectDetails/ProjectOverview.vue"
+    import KeyFeatures from "@/components/ProjectDetails/KeyFeatures.vue"
+    import FutureEnhancements from "@/components/ProjectDetails/FutureEnhancements.vue"
+    import Screenshots from "@/components/ProjectDetails/Screenshots.vue"
+
+    const { id } = useRoute().params
+    const uri = 'https://personal-portfolio-api-cyan.vercel.app/projects/' + id
+
+    const { data: project } = await useFetch(uri, { key: id })
+
+    if (!project.value) {
+    throw createError({ statusCode: 404, statusMessage: 'project not found' })
+    }
+</script>
+
